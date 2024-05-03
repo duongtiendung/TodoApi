d9362d4a5a84145e9fac5622a4759671b500be54
mvn-dungduongt-hn <dung.duongt@monstar-lab.com>
2024-05-03 13:34:56 +0700

    chore: Update project file structure
    The commit updates the project file structure by adding a new project for testing called "TodoApiTest".

---
+++ TodoApiTest/AuthControllerTests.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Claims;
+using TodoApi.Controllers;
+using TodoApi.DTOs;
+using TodoApi.Models;
+using TodoApi.Services.Interfaces;
+
+namespace TodoApiTest
+{
+    public class AuthControllerTests
+    {
+        [Fact]
+        public async Task Register_ValidRequest_ReturnsCreated()
+        {
+            // Arrange
+            var userServiceMock = new Mock<IUserService>();
+            var authController = new AuthController(userServiceMock.Object);
+
+            var loginRequest = new LoginRequest
+            {
+                Username = "TestUser",
+                Password = "TestPassword123"
+            };
+
+            var expectedUser = new User
+            {
+                Id = 1,
+                Username = "TestUser",
+                PasswordHash = "TestPassword123"
+            };
+
+            userServiceMock.Setup(x => x.RegisterUser(It.IsAny<LoginRequest>()))
+                        .ReturnsAsync(expectedUser);
+
+            // Act
+            var result = await authController.Register(loginRequest);
+
+            // Assert
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+            var user = Assert.IsType<User>(createdResult.Value);
+            Assert.Equal("Register", createdResult.ActionName);
+            Assert.Equal(expectedUser.Id, user.Id);
+            Assert.Equal(expectedUser.Username, user.Username);
+            Assert.Equal(expectedUser.PasswordHash, user.PasswordHash);
+        }
+        [Fact]
+        public async Task Register_ValidRequest_ReturnsUsernameisExists()
+        {
+            // Arrange
+            var userServiceMock = new Mock<IUserService>();
+            var authController = new AuthController(userServiceMock.Object);
+
+            var loginRequest = new LoginRequest
+            {
+                Username = "TestUser",
+                Password = "TestPassword123"
+            };
+
+            var expectedUser = new User
+            {
+                Id = 1,
+                Username = "TestUser",
+                PasswordHash = "TestPassword123"
+            };
+            userServiceMock.Setup(x => x.UserNameExists("TestUser")).Returns(true);
+
+            // Act
+            var result = await authController.Register(loginRequest);
+
+            // Assert
+            var exceptionResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+            var exception = Assert.IsType<String>(exceptionResult.Value);
+            Assert.Equal("Username is Exists", exception);
+        }
+        
+
+        [Fact]
+        public async Task Login_ValidRequest_ReturnsOk()
+        {
+            // Arrange
+            var userServiceMock = new Mock<IUserService>();
+            var authController = new AuthController(userServiceMock.Object);
+            var loginRequest = new LoginRequest
+            {
+                // Initialize login request properties
+            };
+
+            var userId = 1; // Simulate user ID
+            var user = new User { Id = userId, Username = "TestUser" }; // Create a User object
+
+            var userClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.UserData, user.Id.ToString(), "userId")
+            };
+
+            var identity = new ClaimsIdentity(userClaims);
+            var principal = new ClaimsPrincipal(identity);
+
+            // Set the User property of the controller context to the mock user
+            authController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = principal }
+            };
+
+            userServiceMock.Setup(x => x.UsersFirstOrDefaultAsync(It.IsAny<LoginRequest>()))
+                          .ReturnsAsync(user);
+            userServiceMock.Setup(x => x.VerifyPasswordHash(loginRequest.Password, user.PasswordHash))
+                          .Returns(true);
+
+            userServiceMock.Setup(x => x.LoginUser(user))
+                           .ReturnsAsync(new LoginResponse
+                           {
+                               // Initialize login response properties
+                           });
+
+            // Act
+            var result = await authController.Login(loginRequest);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var response = Assert.IsType<LoginResponse>(okResult.Value);
+        }
+
+        [Fact]
+        public async Task Login_UserNotFound_ReturnsBadRequest()
+        {
+            // Arrange
+            var userServiceMock = new Mock<IUserService>();
+            var authController = new AuthController(userServiceMock.Object);
+
+            var loginRequest = new LoginRequest
+            {
+                // Initialize login request properties
+            };
+            var userId = 1; // Simulate user ID
+            var user = new User { Id = userId, Username = "TestUser" }; // Create a User object
+
+            var userClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.UserData, user.Id.ToString(), "userId")
+            };
+
+            var identity = new ClaimsIdentity(userClaims);
+            var principal = new ClaimsPrincipal(identity);
+
+            // Set the User property of the controller context to the mock user
+            authController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = principal }
+            };
+
+            User userNull = null;
+            userServiceMock.Setup(x => x.UsersFirstOrDefaultAsync(loginRequest)).ReturnsAsync(userNull);
+
+            // Act
+            var result = await authController.Login(loginRequest);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal("User not found.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task Login_WrongPassword_ReturnsBadRequest()
+        {
+            // Arrange
+            var userServiceMock = new Mock<IUserService>();
+            var authController = new AuthController(userServiceMock.Object);
+
+            var loginRequest = new LoginRequest
+            {
+                // Initialize login request properties
+            };
+            var userId = 1; // Simulate user ID
+            var user = new User { Id = userId, Username = "TestUser" }; // Create a User object
+
+            var userClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.UserData, user.Id.ToString(), "userId")
+            };
+
+            var identity = new ClaimsIdentity(userClaims);
+            var principal = new ClaimsPrincipal(identity);
+
+            // Set the User property of the controller context to the mock user
+            authController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = principal }
+            };
+
+            userServiceMock.Setup(x => x.UsersFirstOrDefaultAsync(It.IsAny<LoginRequest>()))
+                          .ReturnsAsync(user);
+            userServiceMock.Setup(x => x.VerifyPasswordHash(loginRequest.Password, user.PasswordHash))
+                          .Returns(false);
+
+            // Act
+            var result = await authController.Login(loginRequest);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal("Wrong password.", badRequestResult.Value);
+        }
+    }
+}
---
+++ TodoApiTest/TodoesControllerTests.cs
@@ -0,0 +1,255 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApi.Controllers;
+using TodoApi.DTOs;
+using TodoApi.Models;
+using TodoApi.Services.Interfaces;
+using static TodoApi.Models.Enums;
+
+namespace TodoApiTest
+{
+    public class TodoesControllerTests
+    {
+        private readonly Mock<ITodoService> _todoServiceMock;
+        private readonly TodosController _todosController;
+
+        public TodoesControllerTests()
+        {
+            _todoServiceMock = new Mock<ITodoService>();
+            _todosController = new TodosController(_todoServiceMock.Object);
+        }
+        [Fact]
+        public async Task GetTodos_ReturnsOk()
+        {
+            // Arrange
+            var todosList = new List<Todo>
+            {
+                new Todo { Id = 1, Title = "Todo 1", Description = "Description 1", Status = Status.New, DueDate = DateTime.Now.AddDays(1),UserId=1 },
+                new Todo { Id = 2, Title = "Todo 2", Description = "Description 2", Status = Status.Rejected, DueDate = DateTime.Now.AddDays(2) },
+                new Todo { Id = 3, Title = "Todo 3", Description = "Description 3", Status = Status.Pending, DueDate = DateTime.Now.AddDays(3) }
+            };
+
+            _todoServiceMock.Setup(x => x.GetAllTodosAsync(It.IsAny <int>(), It.IsAny<int>(), It.IsAny<int>()))
+                           .ReturnsAsync(todosList);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Name, "TestUser"),
+                new Claim(ClaimTypes.Role, "User") 
+            }));
+
+            // Set the User property of the controller context to the mock user
+            _todosController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+            // Act
+            var result = await _todosController.GetTodos(It.IsAny<int>(), It.IsAny<int>());
+            var okResult = result as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+
+            var anonymousType = okResult.Value.GetType();
+            var todosProperty = anonymousType.GetProperty("Todos");
+            var todos = Assert.IsAssignableFrom<List<Todo>>(todosProperty.GetValue(okResult.Value));
+            Assert.Equal(todosList, todos);
+        }
+
+        [Fact]
+        public async Task GetTodo_ReturnsOkResult_WhenTodoExists()
+        {
+            // Arrange
+            int todoId = 1;
+            int userId = 123;
+            _todoServiceMock.Setup(service => service.GetTodoByIdAsync(todoId, userId))
+                .ReturnsAsync(new Todo { Id = todoId, UserId = userId, Title = "Sample Todo" });
+            var controller = new TodosController(_todoServiceMock.Object);
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.UserData, userId.ToString())
+            }, "mock"));
+
+            // Act
+            var result = await controller.GetTodo(todoId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var todo = Assert.IsType<Todo>(okResult.Value);
+            Assert.Equal(todoId, todo.Id);
+            Assert.Equal(userId, todo.UserId);
+            Assert.Equal("Sample Todo", todo.Title);
+        }
+
+        [Fact]
+        public async Task GetTodo_ReturnsNotFound_WhenTodoDoesNotExist()
+        {
+            // Arrange
+            int todoId = 1;
+            int userId = 123;
+            _todoServiceMock.Setup(service => service.GetTodoByIdAsync(todoId, userId))
+                .ReturnsAsync((Todo)null);
+            var controller = new TodosController(_todoServiceMock.Object);
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.UserData, userId.ToString())
+            }, "mock"));
+
+            // Act
+            var result = await controller.GetTodo(todoId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task PutTodo_WithValidRequest_ReturnsNoContent()
+        {
+            // Arrange
+            var todoRequest = new TodoUpdateRequest { Id = 1, Title = "Updated Todo", Description = "Updated Description" };
+            var userId = 1; // Simulate user ID
+
+            _todoServiceMock.Setup(x => x.UpdateTodoAsync(todoRequest, userId)).ReturnsAsync(true);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Name, "TestUser"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.UserData,userId.ToString(),"userId")
+            }));
+
+            // Set the User property of the controller context to the mock user
+            _todosController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+            // Act
+            var result = await _todosController.PutTodo(todoRequest);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task PutTodo_WithInvalidRequest_ReturnsNotFound()
+        {
+            // Arrange
+            var todoRequest = new TodoUpdateRequest { Id = 1, Title = "Updated Todo", Description = "Updated Description" };
+            var userId = 1; // Simulate user ID
+
+            _todoServiceMock.Setup(x => x.UpdateTodoAsync(todoRequest, userId)).ReturnsAsync(false);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Name, "TestUser"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.UserData,userId.ToString(),"userId")
+            }));
+
+            // Set the User property of the controller context to the mock user
+            _todosController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+            // Act
+            var result = await _todosController.PutTodo(todoRequest);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task PostTodo_WithValidRequest_ReturnsCreatedAtAction()
+        {
+            // Arrange
+            var todoRequest = new TodoRequest { Title = "New Todo", Description = "New Description" };
+            var userId = 1; // Simulate user ID
+            var createdTodo = new Todo { Id = 1, Title = "New Todo", Description = "New Description" };
+
+            _todoServiceMock.Setup(x => x.CreateTodoAsync(todoRequest, userId)).ReturnsAsync(createdTodo);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Name, "TestUser"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.UserData,userId.ToString(),"userId")
+            }));
+
+            // Set the User property of the controller context to the mock user
+            _todosController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+            // Act
+            var result = await _todosController.PostTodo(todoRequest);
+
+            // Assert
+            ActionResult<Todo> actionResult = result;
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(actionResult.Result);
+            Assert.Equal(nameof(TodosController.GetTodo), createdAtActionResult.ActionName);
+            Assert.Equal(createdTodo.Id, createdAtActionResult.RouteValues["id"]);
+            Assert.Equal(createdTodo, createdAtActionResult.Value);
+        }
+
+        [Fact]
+        public async Task DeleteTodo_WithValidId_ReturnsNoContent()
+        {
+            // Arrange
+            var todoId = 1;
+            var userId = 1; // Simulate user ID
+
+            _todoServiceMock.Setup(x => x.DeleteTodoAsync(todoId, userId)).ReturnsAsync(true);
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Name, "TestUser"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.UserData,userId.ToString(),"userId")
+            }));
+
+            // Set the User property of the controller context to the mock user
+            _todosController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+            // Act
+            var result = await _todosController.DeleteTodo(todoId);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteTodo_WithInvalidId_ReturnsNotFound()
+        {
+            // Arrange
+            var todoId = 1;
+            var userId = 1; // Simulate user ID
+
+            _todoServiceMock.Setup(x => x.DeleteTodoAsync(todoId, userId)).ReturnsAsync(false);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Name, "TestUser"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(ClaimTypes.UserData,userId.ToString(),"userId")
+            }));
+
+            // Set the User property of the controller context to the mock user
+            _todosController.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+            // Act
+            var result = await _todosController.DeleteTodo(todoId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
